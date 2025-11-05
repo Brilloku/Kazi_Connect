f43aa43b4f23fb639c99f203caff83e6af85bbea
Brilloku <bryankuria117@gmail.com>
2025-11-05 20:11:02 +0300

    5.1.1

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -40,7 +40,7 @@ export const AuthProvider = ({ children }) => {
       options: {
         data: userData,
         emailRedirectTo: process.env.NODE_ENV === 'production'
-          ? 'https://kazi-connect-five.vercel.app/verify-email'
+          ? 'https://kazi-connect-ecdz.vercel.app/verify-email'
           : 'http://localhost:3000/verify-email'
       }
     });
