b88aebf0beb4d9927a11916511b56c4836e151c8
Brilloku <bryankuria117@gmail.com>
2025-11-19 11:58:33 +0300

    coss-sit support

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -105,7 +105,7 @@ router.post('/login', async (req, res) => {
     res.cookie('backendToken', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
